759d88c4c069452740f91ac80041cebd5d562996
Konrad Michalik <konrad.michalik@xima.de>
2021-01-09 10:09:47 +0100

    [Bugfix] Time ago renewal bug

--- lighthouse_garden/templates/assets/js/time.js
+++ lighthouse_garden/templates/assets/js/time.js
@@ -1,65 +1,67 @@
 document.addEventListener("DOMContentLoaded", function() {
     (function timeAgo(selector) {
 
-    var templates = {
-        prefix: "",
-        suffix: " ago",
-        seconds: "less than a minute",
-        minute: "about a minute",
-        minutes: "%d minutes",
-        hour: "about an hour",
-        hours: "about %d hours",
-        day: "a day",
-        days: "%d days",
-        month: "about a month",
-        months: "%d months",
-        year: "about a year",
-        years: "%d years"
-    };
-    var template = function(t, n) {
-        return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
-    };
+        var templates = {
+            prefix: "",
+            suffix: " ago",
+            seconds: "less than a minute",
+            minute: "about a minute",
+            minutes: "%d minutes",
+            hour: "about an hour",
+            hours: "about %d hours",
+            day: "a day",
+            days: "%d days",
+            month: "about a month",
+            months: "%d months",
+            year: "about a year",
+            years: "%d years"
+        };
+        var template = function(t, n) {
+            return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
+        };
 
-    var timer = function(time) {
-        if (!time)
-            return;
-        time = time.replace(/\.\d+/, ""); // remove milliseconds
-        time = time.replace(/-/, "/").replace(/-/, "/");
-        time = time.replace(/T/, " ").replace(/Z/, " UTC");
-        time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
-        time = new Date(time * 1000 || time);
+        var timer = function(time) {
+            if (!time)
+                return;
+            time = time.replace(/\.\d+/, ""); // remove milliseconds
+            time = time.replace(/-/, "/").replace(/-/, "/");
+            time = time.replace(/T/, " ").replace(/Z/, " UTC");
+            time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
+            time = new Date(time * 1000 || time);
 
-        var now = new Date();
-        var seconds = ((now.getTime() - time) * .001) >> 0;
-        var minutes = seconds / 60;
-        var hours = minutes / 60;
-        var days = hours / 24;
-        var years = days / 365;
+            var now = new Date();
+            var seconds = ((now.getTime() - time) * .001) >> 0;
+            var minutes = seconds / 60;
+            var hours = minutes / 60;
+            var days = hours / 24;
+            var years = days / 365;
 
-        return templates.prefix + (
-                seconds < 45 && template('seconds', seconds) ||
-                seconds < 90 && template('minute', 1) ||
-                minutes < 45 && template('minutes', minutes) ||
-                minutes < 90 && template('hour', 1) ||
-                hours < 24 && template('hours', hours) ||
-                hours < 42 && template('day', 1) ||
-                days < 30 && template('days', days) ||
-                days < 45 && template('month', 1) ||
-                days < 365 && template('months', days / 30) ||
-                years < 1.5 && template('year', 1) ||
-                template('years', years)
-                ) + templates.suffix;
-    };
+            return templates.prefix + (
+                    seconds < 45 && template('seconds', seconds) ||
+                    seconds < 90 && template('minute', 1) ||
+                    minutes < 45 && template('minutes', minutes) ||
+                    minutes < 90 && template('hour', 1) ||
+                    hours < 24 && template('hours', hours) ||
+                    hours < 42 && template('day', 1) ||
+                    days < 30 && template('days', days) ||
+                    days < 45 && template('month', 1) ||
+                    days < 365 && template('months', days / 30) ||
+                    years < 1.5 && template('year', 1) ||
+                    template('years', years)
+                    ) + templates.suffix;
+        };
 
-    var elements = document.getElementsByClassName('timeago');
-    for (var i in elements) {
-        var $this = elements[i];
-        if (typeof $this === 'object') {
-            $this.innerHTML = timer($this.getAttribute('title') || $this.getAttribute('datetime'));
+        var elements = document.getElementsByClassName('timeago');
+        for (var i = 0; i < elements.length; ++i) {
+            var $this = elements[i];
+            if (typeof $this === 'object') {
+                $this.innerHTML = timer($this.getAttribute('data-datetime'));
+            }
         }
-    }
-    // update time every minute
-    setTimeout(timeAgo, 60000);
+        // update time every minute
+        setTimeout(timeAgo, 60000);
 
-})();
+    })();
+
+
 });
